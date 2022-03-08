11ff6d6e959d9864d7105a30a97c5e40d6fe8b0b
Rachel Kaur <rachel.km@outlook.com>
2022-03-08 02:09:46 +0000

    initial commit

---
+++ for_loops.js
@@ -0,0 +1,13 @@
+shoppingList = ["milk", "hummus", "bread", "apples", "coffee", "biscuits"];
+
+// Print first item in shopping list array
+console.log(shoppingList[0]);
+// Print second item in shopping list array
+console.log(shoppingList[1]);
+
+// For everything that's in this list, loop through and print to terminal...
+// Two ways we can write a for loop
+// 1. Classic for loop
+for (i = 0; i < shoppingList.length; i++) {
+    console.log(shoppingList[i]);
+}
